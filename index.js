25e303b9a562f357c492961ed67f74672dffdddb
ralusx <73664928+ralusx@users.noreply.github.com>
2021-11-06 21:02:30 +0100

    Add initial files

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+window.addEventListener('scroll', function(){
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function toggleMenu() {
+  const menuToggle = document.querySelector('.menuToggle');
+  menuToggle.classList.toggle('active');
+}
